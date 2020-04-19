57b4d739b0bc70622ed7796ecca0c2ace3a1e8aa
Jeremy Sim <jsim0809@gmail.com>
2020-04-19 12:35:41 -0700

    Writes schema for Postgres

---
+++ schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE videos(
+  id          text        NOT NULL,
+  upvotes     smallint    DEFAULT 0,
+  downvotes   smallint    DEFAULT 0,
+  reports     smallint    DEFAULT 0,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE learners(
+  id          text        NOT NULL,
+  upvoted     text[],
+  downvoted   text[],
+  reported    text[],
+  PRIMARY KEY (id)
+);
