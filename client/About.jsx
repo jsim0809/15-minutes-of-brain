d8df8a5fe02e355e7c6baf15bb2e2ead4f4aaba2
Jeremy Sim <jsim0809@gmail.com>
2020-04-21 14:06:35 -0700

    Liz birthday feature added.

--- client/About.jsx
+++ client/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const About = () => (
+const About = ({ handleBirthdayClick }) => (
   <span>
 
     <h5 className="text-left">What's this site about?</h5>
@@ -9,7 +9,7 @@ const About = () => (
             This leads to a lot of wasted hours — we're literally too tired to know what to do.</p>
     <p className="text-left"><span className="font-weight-bolder">Brain15</span> is designed to fill those gaps with growth and learning. 
     Hit the button to bring up a random video from our user-curated database, and enjoy. All videos are between 4 and 20 minutes long.</p>
-    <p className="text-left">Watch enough videos, and you might get a surprise.</p>
+    <p className="text-left">Watch enough videos, and you might get a <span onClick={handleBirthdayClick}>surprise</span>.</p>
     <p className="text-left">So go on, press the button. It's big brain time.</p>
 
   </span>
